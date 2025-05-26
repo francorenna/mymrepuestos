cb08644bee5aac961b62cca9e74453b0cb0fbb29
Franco <francorenna@gmail.com>
2025-05-26 12:32:07 -0300

    Header y menú estilizados, animación de logo mejorada y estructura base

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = () => (
+  <header className="main-header">
+    <div className="logo-container">
+      <img src="/logointro.svg" alt="Logo MyM" className="header-logo" />
+    </div>
+    <nav className="main-nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Empresa</a></li>
+        <li><a href="#">Servicios</a></li>
+        <li><a href="#">Marcas</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
---
+++ src/Components/LogoIntro.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import '../Styles/LogoIntro.css';
+
+function LogoIntro({ onFinish }) {
+  const [shrink, setShrink] = useState(false);
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => setShrink(true), 2000);
+    const timer2 = setTimeout(() => onFinish(), 3000);
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, [onFinish]);
+
+  return (
+    <div className={`logo-intro ${shrink ? 'shrink' : ''}`}>
+      <img src="/logointro.svg" alt="Logo MyM" />
+    </div>
+  );
+}
+
+export default LogoIntro;
